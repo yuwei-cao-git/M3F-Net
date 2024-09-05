071098e5a516125a628f25dd12d1ca9185163200
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-09-05 12:38:13 -0700

    'sen2cor & superresolution'

--- run/S2_sen2cor.py
+++ run/S2_sen2cor.py
@@ -3,7 +3,7 @@
 
 # In[1]:
 
-
+import sys
 import os
 import shutil
 import subprocess
@@ -37,7 +37,9 @@ def run_l2a_process(input_folder, output_folder):
         os.makedirs(output_folder)
 
     # Define the L2A_Process command
-    l2a_process_cmd = f"C:/Users/ycao68/Sen2Cor-02.11.00-win64/L2A_Process.bat {input_folder} --tif --output_dir {output_folder}"
+    l2a_process_cmd = '/home/ycao68/Sen2Cor-02.09.00-Linux64/bin/L2A_Process {input_folder} --output_dir {output_folder}'
+    print ("Running...", l2a_process_cmd)
+    os.system(l2a_process_cmd) 
     
     # Run L2A_Process command
     try:
@@ -55,24 +57,21 @@ def delete_directory(directory):
     except OSError as e:
         print(f"Error: {e.filename, e.strerror}")
 
-
-directory = r"D:\rfm_sen"
-for season in ["fall", "spring", "summer", "winter"]:
-    input_folder = f"{directory}/{season}"
-    unzip_folder = f"{input_folder}/unzip"
-    output_folder = f"{input_folder}/L2A"
-    unzip_directory(input_folder, unzip_folder)
-    safe_folders = [
-        f"{unzip_folder}/{file}"
-        for file in os.listdir(unzip_folder)
-        if file.endswith(".SAFE")
-    ]
-    for safe_folder in tqdm(
-        safe_folders, desc="L2A Processing", leave=True, colour="green"
-    ):
-        run_l2a_process(safe_folder, output_folder)
-        delete_directory(safe_folder)
-    delete_directory(unzip_folder)
+input_folder = sys.argv[1]
+unzip_folder = f"{input_folder}/unzip"
+output_folder = f"{input_folder}/L2A"
+unzip_directory(input_folder, unzip_folder)
+safe_folders = [
+    f"{unzip_folder}/{file}"
+    for file in os.listdir(unzip_folder)
+    if file.endswith(".SAFE")
+]
+for safe_folder in tqdm(
+    safe_folders, desc="L2A Processing", leave=True, colour="green"
+):
+    run_l2a_process(safe_folder, output_folder)
+    delete_directory(safe_folder)
+delete_directory(unzip_folder)
 
 
 
