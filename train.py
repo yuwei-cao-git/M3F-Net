10778cafd23527c5118353b6fc92dc1ea3f5a394
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-10-14 22:43:20 -0700

    'try'

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 import wandb
-import time
 from utils.trainer import train
-import argparse
 
 # local machine: wandb login --cloud --relogin
 
-wandb.init(project='M3F-Net', group='9984wag6')
-config = wandb.config
-
 def main():
+    run=wandb.init()
+    config = wandb.config
     # update: 
     # wandb sweep --update ubc-yuwei-cao/M3F-Net/0w8598wd ./conf/config.yaml
     train(config)
+    run.finish()
+
+if __name__ == '__main__':
+    sweep_id = 'ubc-yuwei-cao/M3F-Net/d53a2kjr'  # Replace with your actual sweep ID
+    wandb.agent(sweep_id, function=main)
