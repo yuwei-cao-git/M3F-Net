358887da7cb084dab21395167ad6e01c4ac23e4e
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-11-18 16:20:59 -0800

    'bug'

--- models/pointNext.py
+++ models/pointNext.py
@@ -35,7 +35,7 @@ class PointNextModel(nn.Module):
             nn.Linear(self.config["emb_dims"], 512),
             nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Dropout(self.config["dropout"]),
+            nn.Dropout(self.config["dp_fuse"]),
             nn.Linear(512, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(),
