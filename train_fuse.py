9d9b86f25dcbb5ee2674556089238a503543895e
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-11-22 09:09:00 -0800

    'training params'

--- train_fuse.py
+++ train_fuse.py
@@ -45,37 +45,39 @@ def main():
     parser.add_argument(
         "--batch_size", type=int, default=8, help="Number of epochs to train the model"
     )
-    parser.add_argument("--emb_dims", default=1024)
+    parser.add_argument("--emb_dims", default=768)
     parser.add_argument("--num_points", default=7168)
-    parser.add_argument("--encoder", default="s", choices=["s", "b", "l", "xl"])
+    parser.add_argument("--encoder", default="xl", choices=["s", "b", "l", "xl"])
 
-    parser.add_argument("--fusion_dim", default=256)
-    parser.add_argument("--optimizer", default="adam", choices=["adam", "adamW", "sgd"])
+    parser.add_argument("--fusion_dim", default=128)
+    parser.add_argument(
+        "--optimizer", default="adamW", choices=["adam", "adamW", "sgd"]
+    )
     parser.add_argument(
         "--scheduler",
         default="steplr",
         choices=["plateau", "steplr", "asha", "cosine"],
     )
     parser.add_argument(
-        "--img_lr", type=float, default=0.0001, help="initial learning rate"
+        "--img_lr", type=float, default=5e-4, help="initial learning rate"
     )
     parser.add_argument(
-        "--pc_lr", type=float, default=0.001, help="initial learning rate"
+        "--pc_lr", type=float, default=1e-5, help="initial learning rate"
     )
     parser.add_argument(
-        "--fuse_lr", type=float, default=0.001, help="initial learning rate"
+        "--fuse_lr", type=float, default=1e-5, help="initial learning rate"
     )
-    parser.add_argument("--pc_loss_weight", type=float, default=2.0)
-    parser.add_argument("--img_loss_weight", type=float, default=2.0)
-    parser.add_argument("--fuse_loss_weight", type=float, default=3.0)
+    parser.add_argument("--pc_loss_weight", type=float, default=1.77)
+    parser.add_argument("--img_loss_weight", type=float, default=1.28)
+    parser.add_argument("--fuse_loss_weight", type=float, default=1.11)
     parser.add_argument("--leading_loss", action="store_true")
-    parser.add_argument("--lead_loss_weight", type=float, default=0.1)
+    parser.add_argument("--lead_loss_weight", type=float, default=0.19)
     parser.add_argument("--weighted_loss", default=True)
     parser.add_argument("--patience", type=int, default=10)
     parser.add_argument("--step_size", type=int, default=10)
     parser.add_argument("--momentum", type=float, default=0.9)
     parser.add_argument("--weight_decay", type=float, default=1e-6)
-    parser.add_argument("--dp_fuse", default=0.5)
+    parser.add_argument("--dp_fuse", default=0.7)
     parser.add_argument("--dp_pc", default=0.5)
     parser.add_argument(
         "--use_mf", action="store_true", help="Use multi-fusion (set flag to enable)"
@@ -102,14 +104,14 @@ def main():
     )
     parser.add_argument(
         "--linear_layers_dims",
-        default=[256, 128],
+        default=[1024, 256],
         help="dims used for the superpixels classify head",
     )
     parser.add_argument(
         "--img_transforms", default="compose", choices=["compose", "random", None]
     )
     parser.add_argument("--pc_transforms", default=True)
-    parser.add_argument("--rotate", default=True)
+    parser.add_argument("--rotate", default=False)
     parser.add_argument("--pc_norm", default=True)
     parser.add_argument("--gpus", type=int, default=torch.cuda.device_count())
     parser.add_argument("--num_workers", type=int, default=8)
