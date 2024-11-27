1a487c086ba98fe66e3e18edf6e026a566b3fd76
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-11-27 08:32:13 -0800

    'bug'

--- utils/trainer_fuse.py
+++ utils/trainer_fuse.py
@@ -49,14 +49,14 @@ def train(config):
     # point_logger = PointCloudLogger(trainer=Trainer)
     # Define a checkpoint callback to save the best model
     checkpoint_callback = ModelCheckpoint(
-        monitor="val_fuse_r2",  # Track the validation loss
+        monitor="fuse_val_r2",  # Track the validation loss
         dirpath=chk_dir,
         filename="best-model-{epoch:02d}-{fuse_val_r2:.2f}",
         save_top_k=1,  # Only save the best model
         mode="min",  # We want to minimize the validation loss
     )
     early_stopping = EarlyStopping(
-        monitor="val_fuse_r2",  # Metric to monitor
+        monitor="fuse_val_r2",  # Metric to monitor
         patience=10,  # Number of epochs with no improvement after which training will be stopped
         mode="max",  # Set "min" for validation loss
         verbose=True,
